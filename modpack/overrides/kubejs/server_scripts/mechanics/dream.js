f15b96947ec484f99f21a218517f58ade8120aef
melontini <melontini@yandex.ru>
2022-05-31 13:40:06 +0700

    better bruh moment

--- modpack/overrides/kubejs/server_scripts/mechanics/dream.js
+++ modpack/overrides/kubejs/server_scripts/mechanics/dream.js
@@ -1,12 +1,11 @@
 onEvent('block.right_click', event => {
-    if (event.getWorld().getLocalTime() >= 12500.0) {var cond = true}
-    color.forEach(color => {
-        if (event.getBlock().id == 'minecraft:' + color + '_bed' & cond == true) {
-            if (Math.floor(Math.random() * 7500000000001) == 1) {
+    if (!event.getWorld().isDaytime()) {
+        color.forEach(color => {
+            if (event.getBlock().id == 'minecraft:' + color + '_bed' && Math.floor(Math.random() * 7500000000000) == 1) {
                 event.server.runCommandSilent(`/effect give ${event.getEntity().getDisplayName()} minecraft:luck 1000000 2 true`)
                 event.server.runCommandSilent(`/advancement grant ${event.getEntity().getDisplayName()} only plus:story/dream`)
+                //console.info(event.getWorld().getLocalTime())
             }
-            //console.info(event.getWorld().getLocalTime())
-        }
-    })
+        })
+    }
 })
