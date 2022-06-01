3976c3efc2302269b5e87e6333dd28c0f9c5853f
melontini <melontini@yandex.ru>
2022-06-02 00:43:45 +0700

    fixed some log spam

--- modpack/overrides/kubejs/server_scripts/advancements/husbandry/plant_crops.js
+++ modpack/overrides/kubejs/server_scripts/advancements/husbandry/plant_crops.js
@@ -2,8 +2,10 @@ onEvent('block.place', event => {
     let block = event.getBlock();
     crops.forEach(crop => {
         if (block.id == crop) {
-            event.server.runCommandSilent(`/advancement grant ${event.getEntity().getDisplayName()} only plus:husbandry/plant_seeds`)
-            //console.log(block.id + " successfully planted a seed /nI sure love minecraft not allowing tags in place block advancements")
+            if (event.getEntity() != null) {
+                event.server.runCommandSilent(`/advancement grant ${event.getEntity().getDisplayName()} only plus:husbandry/plant_seeds`)
+                //console.log(block.id + " successfully planted a seed /nI sure love minecraft not allowing tags in place block advancements")
+            }
         }
     })
 })
