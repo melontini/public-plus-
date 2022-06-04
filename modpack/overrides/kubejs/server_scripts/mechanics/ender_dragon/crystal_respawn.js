dbd3e30bb00e0cc00864a5532b3017807a5b58d8
melontini <melontini@yandex.ru>
2022-06-05 00:18:34 +0700

    Respawning crystals This is terrible I should get 5000 years in jail for that

---
+++ modpack/overrides/kubejs/server_scripts/mechanics/ender_dragon/crystal_respawn.js
@@ -0,0 +1,36 @@
+var dragon
+onEvent('entity.spawned', event => {
+    let world = event.getWorld()
+    let entity = event.getEntity()
+    if (entity.getType() == 'minecraft:ender_dragon') {
+        dragon = true
+        event.server.scheduleInTicks(1, event.server, function (callback) {
+            if (!entity.isAlive()) {
+                dragon = false
+            } else {
+                callback.reschedule()
+            }
+        })
+    }
+    if (entity.getType() == 'minecraft:end_crystal') {
+        //console.log(entity.getWorld().getDimension())
+        if (entity.getWorld().getDimension() == 'minecraft:the_end') {
+            event.server.scheduleInTicks(1, event.server, function (callback) {
+                var x = entity.getX()
+                var y = entity.getY()
+                var z = entity.getZ()
+                if (!event.getEntity().isAlive() && dragon != false) {
+                    event.server.scheduleInTicks(2400, event.server, function (callback) {
+                        event.server.runCommandSilent(`/execute in minecraft:the_end run summon minecraft:lightning_bolt ${x} ${y} ${z}`)
+                        console.log(dragon)
+                        event.server.scheduleInTicks(20, event.server, function (callback) {
+                            event.server.runCommandSilent(`/execute in minecraft:the_end run summon minecraft:end_crystal ${x} ${y} ${z}`)
+                        })
+                    })
+                } else {
+                    callback.reschedule()
+                }
+            })
+        }
+    }
+})
