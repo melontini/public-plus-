bf5b1328ada26fc1cbc792c1a1d90ef9f9ecd217
melontini <melontini@yandex.ru>
2022-06-05 00:27:01 +0700

    also this

--- modpack/overrides/kubejs/server_scripts/mechanics/ender_dragon/crystal_respawn.js
+++ modpack/overrides/kubejs/server_scripts/mechanics/ender_dragon/crystal_respawn.js
@@ -23,7 +23,7 @@ onEvent('entity.spawned', event => {
                     event.server.scheduleInTicks(2400, event.server, function (callback) {
                         if (!event.getEntity().isAlive() && dragon != false) {
                             event.server.runCommandSilent(`/execute in minecraft:the_end run summon minecraft:lightning_bolt ${x} ${y} ${z}`)
-                            console.log(dragon)
+                            //console.log(dragon)
                             event.server.scheduleInTicks(20, event.server, function (callback) {
                                 event.server.runCommandSilent(`/execute in minecraft:the_end run summon minecraft:end_crystal ${x} ${y} ${z}`)
                             })
