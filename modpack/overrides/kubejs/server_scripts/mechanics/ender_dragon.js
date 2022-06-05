11386445151bb8940b6f1c542429ca505c741ba2
melontini <melontini@yandex.ru>
2022-06-05 15:05:40 +0700

    based ender dragon pain

--- modpack/overrides/kubejs/server_scripts/mechanics/ender_dragon.js
+++ modpack/overrides/kubejs/server_scripts/mechanics/ender_dragon.js
@@ -4,6 +4,30 @@ onEvent('entity.spawned', event => {
     let entity = event.getEntity()
     if (entity.getType() == 'minecraft:ender_dragon') {
         dragon = true
+        if (entity.isAlive()) {
+            //TODO add some server load checks
+            //pain
+            event.server.scheduleInTicks(1, event.server, (callback) => {
+                callback.data.tell(Text.red('Hi! Little dev message. Under any circumstances DO NOT RESTART the server or DO NOT RELOAD your world. The script managing the dragon runs on pure enthusiasm, so it will break if you restart the game or server.'))
+            })
+            event.server.scheduleInTicks(20, event.server, function (callback) {
+                if (entity.isAlive()) {
+                    var players = event.server.getPlayers()
+                    var playersInEnd = [];
+                    players.forEach(player => {
+                        if (player.getWorld().getDimension() == 'minecraft:the_end') {
+                            if (!playersInEnd.includes(player)) {
+                                playersInEnd.push(player);
+                            }
+                        }
+                    });
+
+                    var playersCountInEnd = playersInEnd.length;
+                    event.server.runCommandSilent(`/attribute ${entity.getId()} minecraft:generic.max_health base set ${(parseInt(Math.sqrt(500 * playersCountInEnd)) * 10)}`)
+                    callback.reschedule()
+                }
+            })
+        }
         event.server.scheduleInTicks(1, event.server, function (callback) {
             if (!entity.isAlive()) {
                 dragon = false
@@ -22,6 +46,7 @@ onEvent('entity.spawned', event => {
                 if (!event.getEntity().isAlive() && dragon != false) {
                     event.server.scheduleInTicks(2400, event.server, function (callback) {
                         if (!event.getEntity().isAlive() && dragon != false) {
+                            event.server.runCommandSilent(`/particle minecraft:end_rod ${x} ${y} ${z} 0.5 0.5 0.5 0.5 100 normal`)
                             event.server.runCommandSilent(`/execute in minecraft:the_end run summon minecraft:lightning_bolt ${x} ${y} ${z}`)
                             //console.log(dragon)
                             event.server.scheduleInTicks(20, event.server, function (callback) {
